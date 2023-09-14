e015a8ae30d6e3ca7c8274c7e82dfe28abac1b58
msj <audtmdwjd@naver.com>
2023-09-14 16:48:33 +0900

    tsconfig

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,11 @@
 // const {username, age }  = {username:"Alice", age: 30}
 // username, age
 
-import { appName, greet, user } from "./module.js";
+import { appName, greet, user } from "./module";
 // 디폴트 모듈
 // 자동완성으로 파일명하고 모듈명을 동일하게 해줌
 // import module from "./module";
-import metadata from "./module.js";
+import metadata from "./module";
 
 const name = "Javascript!!";
 
--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,21 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 /** @type {import('webpack').Configuration} */
 module.exports = {
   // 시작지점의 코드(여기서부터 번들링이 시작)
-  entry: "./src/index.js",
+  entry: "./src/index.ts",
+  // entry부터 시작해서 확장자가 ts/js 인 파일들을 번들링하겠다.
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  // 모듈 해석기
+  module: {
+    rules: [
+      {
+        test: /\.ts$/, // .ts 파일에 대해서
+        use: "ts-loader", // ts-loader를 이용하여 해석하겠다.
+        exclude: /node_modules/, // 예외 디렉터리
+      },
+    ],
+  },
   // 번들링이 완료된 코드를 출력하는 위치
   output: {
     filename: "bundle.js",
